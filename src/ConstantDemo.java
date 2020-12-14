75409ff3d0c09714fe41b03327d641cf5578705a
jeffreycnhub <15157650@qq.com>
2020-12-14 15:39:53 +0800

    Java study2

---
+++ src/ConstantDemo.java
@@ -0,0 +1,33 @@
+public class ConstantDemo {
+
+    /*
+     *  补充两个快捷键：
+     *    main方法的快捷键：psvm
+     *    输出语句的快捷键: sout
+     *
+     * */
+
+    public static void main(String[] args) {
+        //字符串常量
+        System.out.println("abc");
+        System.out.println("123");
+
+        //字符常量
+        System.out.println('a');
+        System.out.println('0');
+
+        //整数常量
+        System.out.println(10);
+        System.out.println(-10);
+
+        //小数常量
+        System.out.println(1.2);
+        System.out.println(-12.6);
+
+        //布尔常量
+        System.out.println(true);
+        System.out.println(false);
+
+
+    }
+}
